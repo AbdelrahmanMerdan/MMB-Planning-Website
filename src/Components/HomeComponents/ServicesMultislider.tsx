67fdb0369c831b27be95750f5933fdaa03a757ce
Abdelrahman Merdan <116974636+AbdelrahmanMerdan@users.noreply.github.com>
2023-09-21 12:20:24 -0400

    Update ServicesMultislider.tsx

--- src/Components/HomeComponents/ServicesMultislider.tsx
+++ src/Components/HomeComponents/ServicesMultislider.tsx
@@ -1,82 +1,65 @@
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-function ServicesMultislider(){
 
-    const carousel = document.querySelector(".carousel")
+function ServicesMultislider() {
+    const carouselRef = useRef<HTMLDivElement | null>(null);
+    let isSlideStart = false, prevPageX: number, prevScrollLeft: number;
 
-    let isSlideStart = false, prevPageX:number, prevScrollLeft:number;
+    useEffect(() => {
+        const carousel = carouselRef.current;
 
-    const slideStart = (e: Event) =>{
-        isSlideStart = true;
-        prevPageX = (e as MouseEvent).pageX;
-        if (carousel) {
-        prevScrollLeft = carousel.scrollLeft;
+        const slideStart = (e: MouseEvent) => {
+            isSlideStart = true;
+            prevPageX = e.pageX;
+            prevScrollLeft = carousel!.scrollLeft;
         }
 
-    }
-
-    const Slide = (e: Event) =>{
-        if(!isSlideStart) return;
-        e.preventDefault();
-        let positionDiff = (e as MouseEvent).pageX - prevPageX
-        
-        if (carousel) {
-            carousel.classList.add('dragging')
-            carousel.scrollLeft = prevScrollLeft - positionDiff;
-        
-    }
-    }
-
-    const slideStop = () =>{
-        isSlideStart = false;
-        if(carousel){
-            carousel.classList.remove('dragging')
+        const Slide = (e: MouseEvent) => {
+            if (!isSlideStart) return;
+            e.preventDefault();
+            let positionDiff = e.pageX - prevPageX;
+            carousel!.scrollLeft = prevScrollLeft - positionDiff;
         }
-    }
 
+        const slideStop = () => {
+            isSlideStart = false;
+        }
 
-    if (carousel) {
-    carousel.addEventListener("mousemove", Slide)
-    carousel.addEventListener("mousedown", slideStart)
-    carousel.addEventListener("mouseup", slideStop)
-    }
+        if (carousel) {
+            carousel.addEventListener("mousemove", Slide);
+            carousel.addEventListener("mousedown", slideStart);
+            carousel.addEventListener("mouseup", slideStop);
+        }
 
-    let firstImg = carousel?.querySelectorAll("img")[0];
+        return () => {
+            if (carousel) {
+                carousel.removeEventListener("mousemove", Slide);
+                carousel.removeEventListener("mousedown", slideStart);
+                carousel.removeEventListener("mouseup", slideStop);
+            }
+        }
+    }, []);
+
     let firstImgWidth: number;
-    
-    if (firstImg) {
-        firstImgWidth = firstImg.clientWidth + 14; 
-    }
-    
-    // const showHideIcons = () => {
-    //     if(carousel){
-    //         const leftarr = document.querySelector("#leftarrowM")
-    //         if(carousel.scrollLeft == 0){
-    //             if(leftarr)(
-    //                 leftarr.setAttribute("style", "display: none;")
-    //             )
-    //         }
-    //         else{
-    //             if(leftarr)(
-    //                 leftarr.setAttribute("style", "display: block;")
-    //             )
-    //         }
-    //     }
-    // }
 
-    const Next = (txt:string) => {
-        if(carousel){
-            if(txt == "left"){
-                carousel.scrollLeft -= firstImgWidth;
+    useEffect(() => {
+        const firstImg = carouselRef.current?.querySelectorAll("img")[0];
+    
+        if (firstImg) {
+            firstImgWidth = firstImg.clientWidth + 14; 
+        }
+    }, []);
+
+    const Next = (txt: string) => {
+        if(carouselRef.current){
+            if(txt === "left"){
+                carouselRef.current.scrollLeft -= firstImgWidth;
             }
             else{
-                carousel.scrollLeft += firstImgWidth;
+                carouselRef.current.scrollLeft += firstImgWidth;
             }
-            // showHideIcons();
         }
-        
     }
-
-
 
     return(
     <>
@@ -85,7 +68,7 @@ function ServicesMultislider(){
     <h1 className="servicesh1">Our Services</h1>
         <div className="wrapper">
             <div className="leftarrowM" id="arrow" onClick= {() => Next("left")}> &#8249; </div>
-            <div  className="carousel">
+            <div ref={carouselRef} className="carousel">
                 <img src="https://i.imgur.com/Ud0gPy0.png" alt="" />
                 <img src="https://i.imgur.com/StYyOuY.png" alt="" />
                 <img src="https://i.imgur.com/ruidpJu.png" alt="" />
